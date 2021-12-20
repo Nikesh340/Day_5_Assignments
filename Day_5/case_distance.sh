13b9bbdef1906a841c666c7f7244cb2053ef6a99
Nikesh340 <nikeshyv@gmail.com>
2021-12-21 01:04:43 +0530

    Day_5_Assignments

---
+++ Day_5/case_distance.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Enter distance (in km) : "
+read km
+
+meter=`echo $km\*1000`
+feet=`echo $meter\*3.2808`
+inches=`echo $feet\*12`
+cm=`echo $feet\*30.48`
+
+echo "Total meter is    : $meter "
+echo "Total feet is     : $feet "
+echo "Total inches is   : $inches "
+echo "Total centimeters : $cm "
